e7ffcaeb337ec1bda8cc606fcabbaa4c88a1c8e9
ryro98 <dawidjaroszewicz@wp.pl>
2023-03-22 11:55:13 +0100

    cash: added POST, PUT and DELETE endpoints + tests

--- cash/src/main/java/com/casino/CashController.java
+++ cash/src/main/java/com/casino/CashController.java
@@ -4,10 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.springframework.http.HttpStatus.NOT_FOUND;
-import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @RequestMapping("api/v1/cash")
@@ -48,5 +48,72 @@ public class CashController {
         } else {
             return ResponseEntity.status(NOT_FOUND).body("Cash with given user id does not exist.");
         }
+    }
+
+    @PostMapping
+    @ResponseStatus(value = CREATED)
+    public ResponseEntity<?> createCash(@RequestParam Integer userId) {
+        if (cashRepository.existsCashByUserId(userId)) {
+            return ResponseEntity.status(CONFLICT).body("Cash with given user id already exists.");
+        }
+        Cash cash = cashService.createCash(userId);
+        return ResponseEntity.status(CREATED).body(cash);
+    }
+
+    @PutMapping("/user/{id}")
+    @ResponseStatus(value = OK)
+    public ResponseEntity<?> addCash(@PathVariable(name = "id") Integer userId) throws CashNotFoundException {
+        if (cashRepository.existsCashByUserId(userId)) {
+            Cash oldCash = Cash.builder()
+                    .cash(cashService.getCashByUserId(userId).getCash())
+                    .build();
+            Cash newCash = cashService.addCash(userId);
+            if (oldCash.getCash() != newCash.getCash()) {
+                return ResponseEntity.status(OK).body(newCash);
+            } else {
+                return ResponseEntity.status(BAD_REQUEST).body(newCash);
+            }
+        } else {
+            return ResponseEntity.status(NOT_FOUND).body("Cash with given user id does not exist.");
+        }
+    }
+
+    @PutMapping("/gamble/{userId}")
+    @ResponseStatus(value = OK)
+    public ResponseEntity<?> gambleCash(
+            @PathVariable(name = "userId") Integer userId,
+            @RequestParam Integer money) throws CashNotFoundException {
+        if (cashRepository.existsCashByUserId(userId)) {
+            Cash oldCash = Cash.builder()
+                    .cash(cashService.getCashByUserId(userId).getCash())
+                    .build();
+            Cash newCash = cashService.gambleCash(userId, money);
+            if (newCash.getCash() != oldCash.getCash()) {
+                return ResponseEntity.status(OK).body(newCash);
+            } else {
+                if (money > oldCash.getCash() * 0.5) {
+                    return ResponseEntity.status(BAD_REQUEST).body("You bet too much cash, try again.");
+                }
+                LocalDateTime time = LocalDateTime.now();
+                LocalDateTime customer_timer = newCash.getGetCashTimer();
+                int timer_now = time.getHour() * 3600 + time.getMinute() * 60 + time.getSecond();
+                int timer_customer = customer_timer.getHour() * 3600 + customer_timer.getMinute() * 60 + customer_timer.getSecond() + 300;
+                return ResponseEntity.status(BAD_REQUEST).body(String.format("Wait %d seconds", timer_customer-timer_now));
+            }
+        } else {
+            return ResponseEntity.status(NOT_FOUND).body("Cash with given user id does not exist.");
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(value = NO_CONTENT)
+    public ResponseEntity<String> deleteCash(@PathVariable(name = "id") Integer id) {
+        if (cashRepository.existsById(id)) {
+            cashService.deleteCash(id);
+            return ResponseEntity.status(NO_CONTENT).body("");
+        } else {
+            return ResponseEntity.status(NOT_FOUND).body("Cash with given id does not exist.");
+        }
+
     }
 }
--- cash/src/main/java/com/casino/CashService.java
+++ cash/src/main/java/com/casino/CashService.java
@@ -3,8 +3,10 @@ package com.casino;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 @Service
@@ -23,5 +25,64 @@ public class CashService {
 
     public Cash getCashByUserId(Integer userId) throws CashNotFoundException {
         return cashRepository.findCashByUserId(userId).orElseThrow(CashNotFoundException::new);
+    }
+
+    public Cash createCash(Integer userId) {
+        Cash cash = Cash.builder()
+                .userId(userId)
+                .cash(0)
+                .getCashTimer(LocalDateTime.now().minusDays(1))
+                .gambleTimer(LocalDateTime.now().minusDays(1))
+                .build();
+        cashRepository.saveAndFlush(cash);
+        return cash;
+    }
+
+    public Cash addCash(Integer userId) throws CashNotFoundException {
+        Cash cash = cashRepository.findCashByUserId(userId).orElseThrow(CashNotFoundException::new);
+
+        LocalDateTime time = LocalDateTime.now();
+        LocalDateTime customer_timer = cash.getGetCashTimer();
+        int timer_now = time.getHour() * 3600 + time.getMinute() * 60 + time.getSecond();
+        int timer_customer = customer_timer.getHour() * 3600 + customer_timer.getMinute() * 60 + customer_timer.getSecond() + 300;
+        if (customer_timer == null || time.getDayOfMonth() != customer_timer.getDayOfMonth() || timer_now > timer_customer) {
+            cash.setCash(cash.getCash() + 500);
+            cash.setGetCashTimer(LocalDateTime.now());
+            cashRepository.saveAndFlush(cash);
+        }
+        return cash;
+    }
+
+    public Cash gambleCash(Integer userId, Integer money) throws CashNotFoundException {
+        Cash cash = cashRepository.findCashByUserId(userId).orElseThrow(CashNotFoundException::new);
+
+        LocalDateTime time = LocalDateTime.now();
+        LocalDateTime customer_timer = cash.getGambleTimer();
+        int timer_now = time.getHour() * 3600 + time.getMinute() * 60 + time.getSecond();
+        int timer_customer = customer_timer.getHour() * 3600 + customer_timer.getMinute() * 60 + customer_timer.getSecond() + 300;
+        if (customer_timer == null || time.getDayOfMonth() != customer_timer.getDayOfMonth() || timer_now > timer_customer) {
+            cash = gamble(cash, money);
+        }
+        return cash;
+    }
+
+    private Cash gamble(Cash cash, Integer money) {
+        if (money <= cash.getCash() * 0.5) {
+            int random = new Random().ints(1, 10)
+                    .findFirst()
+                    .getAsInt();
+            if (random > 5) {
+                cash.setCash(cash.getCash() + money);
+            } else {
+                cash.setCash(cash.getCash() - money);
+            }
+            cash.setGambleTimer(LocalDateTime.now());
+            cashRepository.saveAndFlush(cash);
+        }
+        return cash;
+    }
+
+    public void deleteCash(Integer id) {
+        cashRepository.deleteById(id);
     }
 }
